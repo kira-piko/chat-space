6f06181371f88aeb22b455c6e3c6bfddeaafd727
kira-piko <sasuke.25.niina@gmail.com>
2020-02-15 13:14:44 +0900

    メッセージ送信機能終了

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,8 +1,9 @@
 class GroupsController < ApplicationController
-  
-  def index
-  end
 
+  def index
+
+  end
+  
   def new
     @group = Group.new
     @group.users << current_user
@@ -19,12 +20,13 @@ class GroupsController < ApplicationController
 
   def edit
     @group = Group.find(params[:id])
+    
   end
 
   def update
     @group = Group.find(params[:id])
     if @group.update(group_params)
-      redirect_to root_path, notice: 'グループを更新しました'
+      redirect_to group_messages_path(@group), notice: 'グループを更新しました'
     else
       render :edit
     end
